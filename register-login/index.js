d76f1d01f67a7f56d922a4a7a69dbdcfbf07e4f1
Nil Folquer <nfolquer@gmail.com>
2020-10-08 11:36:52 +0200

    register page added

---
+++ register-login/index.js
@@ -0,0 +1,32 @@
+const registerButton = document.querySelector('#register.type-button');
+const loginButton = document.querySelector('#login.type-button')
+
+function allFilled() {
+    const nom = document.getElementById('nom');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    const dir = document.getElementById('dir');
+    const city = document.getElementById('city');
+    const zip = document.getElementById('zip');
+
+    const form = [nom, email, password, dir, city, zip];
+    for (let elem of form) {
+        if(elem.value == '') {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+document.addEventListener('keyup', () => {
+    const allEntered = allFilled();
+    const sumbitButton = document.querySelector('.button-submit');
+    if(allEntered) {
+        sumbitButton.disabled = false;
+        sumbitButton.className = 'button-submit button-ready';
+    } else {
+        sumbitButton.disabled = true;
+        sumbitButton.className = 'button-submit unclickable';
+    }
+})
